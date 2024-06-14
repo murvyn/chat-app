d9e027c5dd292fc2557e0891db2d763481c4af80
Murvyn <99991623+murvyn@users.noreply.github.com>
2024-06-14 11:44:29 +0000

    messages api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const user = require("./routes/userRoute");
 const chat = require("./routes/chatRoute");
+const message = require("./routes/messageRoute");
 
 const app = express();
 require("dotenv").config();
@@ -12,6 +13,7 @@ app.use(cors());
 
 app.use("/api/users", user);
 app.use("/api/chats", chat);
+app.use("/api/messages", message);
 
 const port = process.env.PORT || 5000;
 const mongodb_uri = process.env.MONGODB_URI;
